8d755a845bbf756687256317b91f831e1e7aef1b
unknown <matheusm.projetos@gmail.com>
2026-04-03 14:59:26 -0300

    feat: Adiciona Serilog para rastreio e auditoria de erros

--- MyWallpaperEngine/App.xaml.cs
+++ MyWallpaperEngine/App.xaml.cs
@@ -1,15 +1,38 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Reflection.Metadata;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Threading;
+using Serilog;
 
 namespace MyWallpaperEngine
 {
     public partial class App : System.Windows.Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //Configura arquivo de log ao iniciar o aplicativo
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+
+                //Configura o log para ser salvo em arquivos diários na pasta "Logs" dentro do diretório do aplicativo
+                .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("===============================================");
+            Log.Information("MyWallpaperEngine iniciado.");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information("MyWallpaperEngine encerrado.");
+            Log.CloseAndFlush();
+            base.OnExit(e);
+        }
+
         private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
+            Log.Fatal(e.Exception, "Erro Crítico");
+
             System.Windows.MessageBox.Show(
                 $"Atenção!! Ocorreu um erro na execução do programa.\n\n Detalhe técnico: {e.Exception.Message}",
                 "MyWallpaperEngine - Erro Inesperado",
